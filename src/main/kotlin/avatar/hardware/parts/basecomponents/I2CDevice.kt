de47fa717a009d6ffdcc02a1975da2ff3dd2d8de
alexneikoaccedo <128390702+alexneikoaccedo@users.noreply.github.com>
2025-05-21 22:32:34 -0400

    init device ::part3

--- src/main/kotlin/avatar/hardware/parts/basecomponents/I2CDevice.kt
+++ src/main/kotlin/avatar/hardware/parts/basecomponents/I2CDevice.kt
@@ -4,6 +4,7 @@ package avatar.hardware.parts.basecomponents
 import brain.data.local.I2CDeviceConfiguration
 import com.pi4j.context.Context
 import com.pi4j.io.i2c.I2C
+import java.io.IOException
 import java.time.Duration
 
 abstract class I2CDevice(pi4j: Context, i2CDeviceConfiguration: I2CDeviceConfiguration) {
@@ -40,6 +41,29 @@ abstract class I2CDevice(pi4j: Context, i2CDeviceConfiguration: I2CDeviceConfigu
 
     protected fun write(data: Byte) {
         i2c.write(data)
+    }
+
+    /**
+     * Reads the content of the reg register (8bits),
+     * and returns this value in the 0..255 interval if read operation was successfull.
+     * A negative number is returned for an error.
+     * @param reg the local address on the i2c device where the data must be read.
+     * @return the byte value read on the device: a positive number in the 0..255 interval if reading was successful;
+     * a negative number (<0) if reading failed.
+     * @see com.pi4j.io.i2c.I2CDevice.read
+     * @see I2CComponent.readSignedRegisterValue
+     */
+    fun readUnsignedRegisterValue(reg: Int): Int {
+        try {
+            val result: Int = i2c.readRegisterWord(reg)
+            if (result < 0) println("Error when reading i2c register content. Error=$result")
+            return result
+        } catch (e: IOException) {
+            throw Exception(
+                "The content of the register " + reg + " can not be read from the i2c device or i2c bus.",
+                e
+            )
+        }
     }
 
     protected fun sendCommand(command: Byte, data: Byte) {
